8add2e7198d8b71a697b165a3067d0a04647f8a2
jupanaos <julie.pariona@gmail.com>
2023-02-20 18:20:17 +0100

    style: import fonts

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,11 +1,14 @@
 import Footer from "./Footer";
 import Navbar from "./Navbar";
+import { philosopher } from '@/utils/fonts';
 
 const Layout = ({ children }) => {
     return (
-        <div className="content">
+        <div className={`content ${philosopher.className}`}>
             <Navbar />
+            <main>
                 { children }
+            </main>
             <Footer />
         </div>
     );
