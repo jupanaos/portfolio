bb25bb4b38203a2652abcf9ef17dadf76fb55571
jupanaos <julie.pariona@gmail.com>
2023-04-11 11:19:35 +0200

    build: export config for prod on cpanel

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    // Images API optimization don't work on shared hosting for deployment
+    unoptimized: true,
+  },
+  output: 'export',
+  distDir: 'dist',
 }
 
 module.exports = nextConfig
